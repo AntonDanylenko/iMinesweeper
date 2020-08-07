bba28a57d51056baa9aa2ac1ba9e0005e044ff9d
Anton D <adanylenko29@gmail.com>
2020-08-07 13:11:42 -0400

    numbers placed, different difficulties added

--- static/js/game.js
+++ static/js/game.js
@@ -14,23 +14,27 @@ var CELL_SIZE = context.canvas.width/CELL_COLS;
 // console.log(context.canvas.width);
 
 //TILE ICON
-var tilePath = 'static/img/tile.png';
+var tile_path = 'static/img/tile.png';
 var tile = new Image();
-tile.src = tilePath;
+tile.src = tile_path;
 
 //MINE ICON
-var minePath = 'static/img/mine.png';
+var mine_path = 'static/img/mine.png';
 var mine = new Image();
-mine.src = minePath;
+mine.src = mine_path;
 
+//NUMBER COLORS ARRAY
+var num_colors = ["#0000FF", "#008000", "#FF0000", "#00008B", "#800020", "#008080", "#000000", "#808080"];
+
+//PLACE FIELD
 function placeField(){
   init_board_temp = document.getElementById("game").innerHTML.split("/");
   init_board = new Array(0);
   for (var row=0; row<CELL_ROWS; row++){
     init_board.push(init_board_temp[row].split(","));
   }
-  console.log("init_board");
-  console.log(init_board);
+  // console.log("init_board");
+  // console.log(init_board);
 
   //Draw the covered field.
   for (var row=0; row<CELL_ROWS; row++){
@@ -39,12 +43,36 @@ function placeField(){
     }
   }
 
-  //Draw the mines.
+  //Draw the mines and numbers.
   for (var row=0; row<CELL_ROWS; row++){
     for (var col=0; col<CELL_COLS; col++){
       if (init_board[row][col]=="9"){
         context.drawImage(mine, col*CELL_SIZE, row*CELL_SIZE, CELL_SIZE, CELL_SIZE);
       }
+      else if (init_board[row][col]!="0"){
+        context.font = Math.floor(CELL_SIZE*.66) + "px Arial";
+        context.fillStyle = num_colors[parseInt(init_board[row][col],10)-1];
+        context.fillText(init_board[row][col], (col+.3)*CELL_SIZE, (row+.75)*CELL_SIZE);
+      }
     }
   }
+}
+
+
+//CHANGE DIFFICULTY
+function difficulty(dif){
+  var difs = ["10x10", "20x20", "40x40"];
+  for (var x=0; x<3; x++){
+    document.getElementById(difs[x]).className = "";
+  }
+  document.getElementById(difs[dif]).className = "active";
+
+  CELL_ROWS = 10*Math.pow(2,dif);
+  CELL_COLS = CELL_ROWS;
+  CELL_TOTAL = CELL_ROWS*CELL_COLS;
+  CELL_SIZE = context.canvas.width/CELL_COLS;
+  // console.log("CELL INFO");
+  // console.log(CELL_ROWS);
+  // console.log(CELL_SIZE);
+  placeField();
 }
--- static/js/minesweeper.js
+++ static/js/minesweeper.js
@@ -6,9 +6,20 @@ var CELL_TOTAL = 100;
 var MINE_MAX = CELL_TOTAL/10;
 var field = new Array(0);
 
-function generateMinesweeper(){
+//GENERATE MINESWEEPER
+function generateMinesweeper(dif){
+  CELL_ROWS = 10*Math.pow(2,dif);
+  CELL_COLS = CELL_ROWS;
+  CELL_TOTAL = CELL_ROWS*CELL_COLS;
+  MINE_MAX = CELL_TOTAL/10;
+  // console.log("CELL INFO");
+  // console.log(CELL_ROWS);
+  // console.log(CELL_COLS);
+
   document.getElementById("mines_left").innerHTML = MINE_MAX;
 
+  //MAKE EMPTY FIELD
+  field = new Array(0);
   for (var row=0; row<CELL_ROWS; row++){
     var temp = [];
     for (var col=0; col<CELL_COLS; col++){
@@ -17,10 +28,11 @@ function generateMinesweeper(){
     field.push(temp);
   }
 
+  //PLACE MINES
   var mine = 0;
   while (mine<MINE_MAX){
     var random_cell = Math.floor(Math.random() * CELL_TOTAL);
-    console.log(random_cell);
+    // console.log(random_cell);
     var row = Math.floor(random_cell/CELL_ROWS);
     var col = random_cell%CELL_ROWS;
     if (field[row][col] == 0){
@@ -29,11 +41,22 @@ function generateMinesweeper(){
     }
   }
 
+  //PLACE NUMBERS
+  for (var row=0; row<CELL_ROWS; row++){
+    for (var col=0; col<CELL_COLS; col++){
+      if (field[row][col] == 0){
+        field[row][col] = numBombsIn3x3Area(row, col);
+      }
+    }
+  }
+
+  //PRINT FIELD
   console.log("FIELD");
   console.log(field);
 
 
 
+  //CONVERT FIELD ARRAY TO STRING
   var field_string = "";
   for (var row=0; row<CELL_ROWS; row++){
     var temp = "";
@@ -44,6 +67,43 @@ function generateMinesweeper(){
     field_string+=temp.slice(0,-1);
     field_string+="/";
   }
-  console.log(field_string);
+  // console.log(field_string);
   document.getElementById("game").innerHTML = field_string;
+}
+
+
+//HELPER FUNCTIONS
+function numBombsIn3x3Area(row, col){
+  // console.log("ROW: " + row);
+  // console.log("COL: " + col);
+  var num = 0;
+  if (row>0){
+    if (col>0 && field[row-1][col-1] == 9){
+      num++;
+    }
+    if (field[row-1][col] == 9){
+      num++;
+    }
+    if (col<CELL_COLS-1 && field[row-1][col+1] == 9){
+      num++;
+    }
+  }
+  if (col>0 && field[row][col-1] == 9){
+    num++;
+  }
+  if (col<CELL_COLS-1 && field[row][col+1] == 9){
+    num++;
+  }
+  if (row<CELL_ROWS-1){
+    if (col>0 && field[row+1][col-1] == 9){
+      num++;
+    }
+    if (field[row+1][col] == 9){
+      num++;
+    }
+    if (col<CELL_COLS-1 && field[row+1][col+1] == 9){
+      num++;
+    }
+  }
+  return num;
 }
